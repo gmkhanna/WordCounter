f6e77873dd0a5a278fba1017b7a958492a32ad73
Grinil Khanna <gmkhanna@gmail.com>
2017-02-17 10:12:35 -0800

    Add finction to test 1 and 2.

--- Tests/WordCounterTest.cs
+++ Tests/WordCounterTest.cs
@@ -7,55 +7,55 @@ namespace WordCounter
 {
   public class RepeatCounterTest
   {
-    // [Fact]
-    // public void Test1_InputA_True()
-    // {
-    //   //Arrange
-    //   string userInput = "A";
-    //   string checkText = "A";
-    //   bool expResult = true;
-    //
-    //   //Act
-    //   RepeatCounter testMethod = new RepeatCounter(userInput, checkText);
-    //   bool result = testMethod.CountRepeatsBool();
-    //
-    //   //Assert
-    //   Assert.Equal(expResult, result);
-    // }
-    //
-    // [Fact]
-    // public void Test2_InputAT_True()
-    // {
-    //   //Arrange
-    //   string userInput = "AT";
-    //   string checkText = "AT";
-    //   bool expResult = true;
-    //
-    //   //Act
-    //   RepeatCounter testMethod = new RepeatCounter(userInput, checkText);
-    //   bool result = testMethod.CountRepeatsBool();
-    //
-    //   //Assert
-    //   Assert.Equal(expResult, result);
-    // }
-
     [Fact]
-    public void Test3_InputTEA_TEATEA()
+    public void Test1_InputA_True()
     {
       //Arrange
-      string userInput = "TEA";
-      string checkText = "TEA";
-      bool expResult = "T","E","A","T","E","A";
+      string userInput = "A";
+      string checkText = "A";
+      bool expResult = true;
 
       //Act
       RepeatCounter testMethod = new RepeatCounter(userInput, checkText);
-      bool result = testMethod.stringToCharCompare();
+      bool result = testMethod.CountRepeatsBool();
 
       //Assert
-      Console.WriteLine(expResult);
-      Console.WriteLine(result);
       Assert.Equal(expResult, result);
     }
+
+    [Fact]
+    public void Test2_InputAT_True()
+    {
+      //Arrange
+      string userInput = "BUFFALO";
+      string checkText = "BUFFALO";
+      bool expResult = true;
+
+      //Act
+      RepeatCounter testMethod = new RepeatCounter(userInput, checkText);
+      bool result = testMethod.CountRepeatsBool();
+
+      //Assert
+      Assert.Equal(expResult, result);
+    }
+
+    // [Fact]
+    // public void Test3_InputTEA_TEATEA()
+    // {
+    //   //Arrange
+    //   string userInput = "TEA";
+    //   string checkText = "TEA";
+    //   bool expResult = "T","E","A","T","E","A";
+    //
+    //   //Act
+    //   RepeatCounter testMethod = new RepeatCounter(userInput, checkText);
+    //   bool result = testMethod.stringToCharCompare();
+    //
+    //   //Assert
+    //   Console.WriteLine(expResult);
+    //   Console.WriteLine(result);
+    //   Assert.Equal(expResult, result);
+    // }
 
 
   }
